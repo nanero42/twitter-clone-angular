6eec25e6c8f560b3bf00f939bb7c13b3d9a63011
Nariman <n.murzakaev@big3.ru>
2023-03-10 18:14:42 +0300

    reinit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
+  { path: '**', loadChildren: () => import('./pages/error-page/error-page.module').then(m => m.ErrorPageModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
