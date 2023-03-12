6fe50d880c5278f0187e6a4668eb58be5b17e8fc
Nariman <n.murzakaev@big3.ru>
2023-03-12 22:45:10 +0300

    updated navbar-low component

--- src/app/component/navbar-low/navbar-low.component.ts
+++ src/app/component/navbar-low/navbar-low.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { EIcons } from 'src/app/enums';
 
 @Component({
   selector: 'app-navbar-low',
@@ -7,5 +8,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarLowComponent {
-
+  eIcon = EIcons;
 }
