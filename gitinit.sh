448aa1dbfea9cc91f2d9f8a58e54acbac207fe3c
Nariman <n.murzakaev@big3.ru>
2023-03-13 21:30:02 +0300

    feat(gitinit.sh) added git aliases

---
+++ gitinit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+git config --replace-all alias.ls '!git log --oneline'
+git config --replace-all alias.m '!f() {
+  git add . &&
+  git commit -m $1
+}; f'
+git config --replace-all alias.p '!f() {
+  git add . &&
+  git commit -m "$1" &&
+  git push --set-upstream origin issue/$2;
+}; f'
+git config --replace-all alias.r1 '!git reset HEAD~1'
