eb2bca2c6407ad6c11c0118372d70465b7d0fdd8
Nariman <n.murzakaev@big3.ru>
2023-03-13 12:45:11 +0300

    feat(navbar-low.component) added dynamic icon changes depending on whether element pressed by user or nit

--- src/app/enums/index.ts
+++ src/app/enums/index.ts
@@ -2,9 +2,13 @@ export enum EIcons {
   logo = 'logo',
   avatar = 'avatar',
   home = 'home',
+  homeActive = 'homeActive',
   search = 'search',
+  searchActive = 'searchActive',
   notification = 'notification',
+  notificationActive = 'notificationActive',
   message = 'message',
+  messageActive = 'messageActive',
   createPost = 'createPost',
 }
 
