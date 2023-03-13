71f687d13bcf87f9ad3366fde9058d6fc4dfe190
Nariman <n.murzakaev@big3.ru>
2023-03-13 21:53:58 +0300

    refactor(git-alias.sh) refactored code generator and renamed linked files

--- git-alias.sh
+++ git-alias.sh
@@ -3,11 +3,11 @@
 git config --replace-all alias.ls '!git log --oneline'
 git config --replace-all alias.m '!f() {
   git add . &&
-  git commit -m $1
+  git commit -m "$1"
 }; f'
 git config --replace-all alias.p '!f() {
   git add . &&
   git commit -m "$1" &&
-  git push --set-upstream origin issue/$2;
+  git push --set-upstream origin "$2";
 }; f'
 git config --replace-all alias.r1 '!git reset HEAD~1'
