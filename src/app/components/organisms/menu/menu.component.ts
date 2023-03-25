0e97e209f95fe83194d2c1f2ac1bf8fd6df65d19
Nariman <n.murzakaev@big3.ru>
2023-03-25 15:36:42 +0300

    refactor(organisms) renamed all organisms

--- src/app/components/organisms/menu/menu.component.ts
+++ src/app/components/organisms/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { IAccordionItem } from 'src/app/interfaces';
 
 @Component({
-  selector: 'app-menu',
+  selector: 'organism-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
