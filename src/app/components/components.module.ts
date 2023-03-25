840dc1ed002c92ebbe0f7ca7aa2a1cb42ef2a77b
Nariman <n.murzakaev@big3.ru>
2023-03-25 14:09:17 +0300

    delete(button.component) deleted button component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { IconComponent } from './icon/icon.component';
-import { ButtonComponent } from './button/button.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { MenuComponent } from './menu/menu.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 const exportedComponents = [
-  ButtonComponent,
   IconComponent,
   MenuComponent,
   TabsComponent,
